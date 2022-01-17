0d7681e09b4dfbcb79a3d91e56af0197fc003fa9
John Boesen <jmboesen@college.harvard.edu>
2022-01-16 18:07:18 -0600

    added api-function folder

--- src/api-functions.js
+++ src/api-functions.js
@@ -1,1 +1,12 @@
-const http = require("http");
+const requestAuthorization = () => {
+    // TODO: get id's from backend
+    const id = '4aa2e2d16efe46e198d444f232e96695'; // client id
+    const sec = '42147b97f5254fc1b06949d1cc3f0694'; // secret
+    const redirect_uri = 'http://localhost:3000'; // feel free to edit
+    url += "?client_id=" + id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
--- src/containers/settings.js
+++ src/containers/settings.js
@@ -1,16 +1,5 @@
-require('react')
+import React from 'react'
 
-const requestAuthorization = () => {
-    client_id = document.getElementById("clientId").value;
-    client_secret = document.getElementById("clientSecret").value;
-    localStorage.setItem("client_id", client_id);
-    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
-
-    let url = AUTHORIZE;
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&show_dialog=true";
-    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
-    window.location.href = url; // Show Spotify's authorization screen
+const Settings = () => {
+    return (<p>hello</p>)
 }
