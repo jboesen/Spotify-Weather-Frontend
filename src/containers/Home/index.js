9639eabbb1e98c2cb9b06fe027c860a5b4775664
John Boesen <jmboesen@college.harvard.edu>
2022-01-17 13:53:33 -0600

    added containers

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,21 +3,14 @@ import axios from 'axios'
 // import { credentials } from '../../credentials'
 import { requestAuthorization, onPageLoad } from '../../api-functions'
 
-const Settings = () => {
+const Home = () => {
     const [loggedIn, setLoggedIn] = useState(false)
     // check token
     // spotify = credentials()
-    useEffect(
-        () => { setLoggedIn(onPageLoad) }
-        , []);
-
-    return (<div>
-        {loggedIn ?
-            <p>logged in</p>
-            :
-            <button onClick={requestAuthorization}>click to login to spotify</button>}
-    </div>)
+    useEffect({
+        onPageLoad
+    }, [])
+    return (<button onClick={requestAuthorization}> Click to add spotify</ button>)
 }
-
 
 export default Settings
