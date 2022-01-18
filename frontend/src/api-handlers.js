1fe72832d647180692ee0951fec0f3db0ac693e7
John Boesen <jmboesen@college.harvard.edu>
2022-01-18 12:07:19 -0600

    api

--- frontend/src/api-handlers.js
+++ frontend/src/api-handlers.js
@@ -1,11 +1,25 @@
-const SpotifyWebApi = require('spotify-web-api-node');
+let id = '4aa2e2d16efe46e198d444f232e96695'; // client id
+let sec = '42147b97f5254fc1b06949d1cc3f0694'; // secret
+const redirect_uri = 'http://localhost:3000/home'; // feel free to edit
 
-// why is this being ignored???
-export const api = new SpotifyWebApi({
-    clientId: '4aa2e2d16efe46e198d444f232e96695',
-    clientSecret: '42147b97f5254fc1b06949d1cc3f0694',
-    redirectUri: 'http://localhost:3000'
-});
+let access_token = null;
+let refresh_token = null;
+let currentPlaylist = "";
+
+// Endpoints
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token";
+const PLAYLISTS = "https://api.spotify.com/v1/me/playlists";
+const DEVICES = "https://api.spotify.com/v1/me/player/devices";
+const PLAY = "https://api.spotify.com/v1/me/player/play";
+const PAUSE = "https://api.spotify.com/v1/me/player/pause";
+const NEXT = "https://api.spotify.com/v1/me/player/next";
+const PREVIOUS = "https://api.spotify.com/v1/me/player/previous";
+const PLAYER = "https://api.spotify.com/v1/me/player";
+const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
+const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
+const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
+
 
 const randString = (length) => {
     let text = '';
@@ -18,57 +32,93 @@ const randString = (length) => {
 };
 
 export const requestAuth = () => {
-    const scopes = ['user-read-private', 'playlist-read-private', 'playlist-modify-private'],
-        redirectUri = 'http://localhost:3000/home',
-        clientId = '5fe01282e44241328a84e7c5cc169165',
-        state = randString(16);
-    // Create the authorization URL
-    const authorizeURL = api.createAuthorizeURL(scopes, state)
-    window.location.href = authorizeURL
+    // TODO: get id's from backend
+    let url = AUTHORIZE;
+    url += "?client_id=" + id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
 }
 
-const handleRedirect = () => {
-
-    // The code that's returned as a query parameter to the redirect URI
-    var code = getCode();
-
-    // Retrieve an access token and a refresh token
-    api.authorizationCodeGrant(code).then(
-        function (data) {
-            console.log('The token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-            console.log('The refresh token is ' + data.body['refresh_token']);
-
-            // Set the access token on the API object to use it in later calls
-            api.setAccessToken(data.body['access_token']);
-            api.setRefreshToken(data.body['refresh_token']);
-        },
-        function (err) {
-            console.log('Something went wrong!', err);
-        }
-    );
-}
 
 const getCode = () => {
     let code = null;
     const queryString = window.location.search;
+    if (!queryString) {
+        return null;
+    }
     if (queryString.length > 0) {
         const urlParams = new URLSearchParams(queryString);
         code = urlParams.get('code')
     }
+    localStorage.setItem('sptCode', code)
     return code;
 }
 
-export const refresh = () => {
-    api.refreshAccessToken().then(
-        (data) => {
-            console.log('The access token has been refreshed!');
+const fetchAccessToken = (code) => {
+    console.log("fetchAccessToken")
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&id=" + id;
+    body += "&sec=" + sec;
+    callAuthorizationApi(body);
+}
 
-            // Save the access token so that it's used in future calls
-            api.setAccessToken(data.body['access_token']);
-        },
-        (err) => {
-            console.log('Could not refresh access token', err);
-        }
+export const callAuthorizationApi = (body) => {
+    console.log("callAuthorizationApi")
+    let xhr = new XMLHttpRequest({ mozSystem: true }
     );
+    xhr.open("POST", TOKEN);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + (id + ":" + sec).toString('base64'));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+export const handleAuthorizationResponse = () => {
+    console.log("handleAuthorizationResponse")
+    if (this.status === 200) {
+        const data = JSON.parse(this.responseText);
+        console.log(data);
+        if (data.access_token !== undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token !== undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        // onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+
+
+export const handleRender = () => {
+    // check in url
+    let code;
+    const uriCode = getCode()
+    if (uriCode) {
+        code = uriCode;
+    }
+    // check in local storage
+    else if (localStorage.getItem('sptCode')) {
+        // idk if this will work with the code over and over
+        code = localStorage.getItem('sptCode')
+    }
+    if (code) {
+        fetchAccessToken(code);
+        window.history.pushState("", "", redirect_uri); // remove param from url    
+    }
+    else {
+        requestAuth()
+        return
+    }
 }
--- frontend/src/containers/Home/index.js
+++ frontend/src/containers/Home/index.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { requestAuth, refresh } from '../../api-handlers'
+import { requestAuth, handleRender } from '../../api-handlers'
 
 const Home = () => {
     const history = useHistory()
     const token = localStorage.getItem('token');
-    // how to see if auth'd olr
-    useEffect(refresh(), [])
-    if (!token) {
-        history.push('/');
-    }
+    // how to see if auth'd olr:
+    // check if logged in in session --> consult makeratplay for this
+    // if not logged in, check for code --> getCode
+    // else, prompt login --> requestAuth
+    useEffect(handleRender, [])
+    // if (!token) {
+    //     history.push('/');
+    // }
 
     return (
         <div>
             <p>Congrats, you are logged in!</p>
-            <button onClick={requestAuth()}>Login into Spotify</button>
+            <button onClick={requestAuth}>Login into Spotify</button>
         </div>
     )
 }
